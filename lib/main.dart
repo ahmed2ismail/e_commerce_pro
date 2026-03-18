cb34f69df34cf0551c23340599df4185dfc04219
Ahmed Ismail <ah.hany5050@gmail.com>
2026-03-18 16:55:08 +0200

    chore: add essential packages for networking, storage, and state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const EcommercePro());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode, // هيشتغل بس وأنت بتجرب، ومش هيظهر في النسخة النهائية
+      builder: (context) => EcommercePro(), 
+    ),
+  );
 }
 
 class EcommercePro extends StatelessWidget {
@@ -10,6 +17,10 @@ class EcommercePro extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+    );
   }
 }
