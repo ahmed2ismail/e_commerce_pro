e253faebcdc09cd36d53b9cec70bfa146b5fb10b
Ahmed Ismail <ah.hany5050@gmail.com>
2026-03-18 15:26:53 +0200

    chore: initial project setup with clean architecture folders

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcommercePro());
+}
+
+class EcommercePro extends StatelessWidget {
+  const EcommercePro({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
